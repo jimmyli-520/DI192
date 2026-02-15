fef4b0d0275896ebe9aef65a39c32e852daad66d
Jimmy Li <jimmyli@Jimmys-MacBook-Pro-2.local>
2026-02-15 16:49:17 +0200

    added excerciseXP.py

--- Python/week 1/Day1/ExcerciseXP/excerciseXP.py
+++ Python/week 1/Day1/ExcerciseXP/excerciseXP.py
@@ -1,49 +1,49 @@
 #1 hello world
-# print("Hello world\nHello world\nHello world\nHello world\nHello world")
+print("Hello world\nHello world\nHello world\nHello world\nHello world")
 
 #2 Some Math
-# reuslt = (99**3) * 8 #=7762392
-# print(reuslt)
+reuslt = (99**3) * 8 #=7762392
+print(reuslt)
 
 #3 What is the Output?
-# print(5 < 3) #false
-# print(3 == 3) #true
-# print(3 == "3") #false
-# print("3" > 3) #false
-# print("Hello" == "hello") #false 
+print(5 < 3) #false
+print(3 == 3) #true
+print(3 == "3") #false
+print("3" > 3) #false
+print("Hello" == "hello") #false 
 
 #4 Your computer Brand
-# computer_brand = "Macbook"
-# print(f"I have a {computer_brand}.computer")
+computer_brand = "Macbook"
+print(f"I have a {computer_brand}.computer")
 
 #5 Your information
-# name = "Jimmy"
-# age = 37
-# shoe_size = 41
-# info = (f"My name is {name}, I am {age} years old, my shoe size is {shoe_size}, and i like to code.")
-# print(info)
+name = "Jimmy"
+age = 37
+shoe_size = 41
+info = (f"My name is {name}, I am {age} years old, my shoe size is {shoe_size}, and i like to code.")
+print(info)
 
 #6 A&B
-# a = 8
-# b = 6
-# if a > b:
-#   print("Hello World")
+a = 8
+b = 6
+if a > b:
+  print("Hello World")
 
 #7 Odd or Even
-# user_input = input("Enter a number: ")
-# number = int(user_input)
-# if (number % 2) == 0:
-#   print(f"The number {number} is even")
-# else:
-#   print(f"The number {number} is odd")
+user_input = input("Enter a number: ")
+number = int(user_input)
+if (number % 2) == 0:
+  print(f"The number {number} is even")
+else:
+  print(f"The number {number} is odd")
 
 #8 What's your name?
-# my_name = "Jimmy"
-# user_name = input("Enter your name: ")
-# if user_name == my_name:
-#   print("I guess you'll have to change it then.")
-# else:
-#   print("I guess the cool names are taken.")
+my_name = "Jimmy"
+user_name = input("Enter your name: ")
+if user_name == my_name:
+  print("I guess you'll have to change it then.")
+else:
+  print("I guess the cool names are taken.")
 
 #9 Tall enough to Ride A Roller Coaster
 user_height = input("What's your height in cm?")
