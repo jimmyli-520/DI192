24c8bbf427ec6556877c63c19946ad856fe3c8e8
Jimmy Li <jimmyli@Jimmys-MacBook-Pro-2.local>
2026-02-16 17:53:40 +0200

    added dc2.py

---
+++ Python/week 1/Day 2/dc2.py
@@ -0,0 +1,15 @@
+# number = int(input("Enter a number: "))
+# length = int(input("Enter a length "))
+# multiples = []
+
+# for i in range(1, length + 1):
+#     multiples.append(number * i)
+#     print(multiples)
+
+
+word = input("Enter a string: ")
+new_word = ""
+for char in word:
+    if not new_word or char != new_word[-1]:
+        new_word += char
+print(new_word)
