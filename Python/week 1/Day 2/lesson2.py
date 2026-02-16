d053d6f1e91610f2520696a32c453f59b476adc7
Jimmy Li <jimmyli@Jimmys-MacBook-Pro-2.local>
2026-02-16 13:18:22 +0200

    added Day2

---
+++ Python/week 1/Day 2/lesson2.py
@@ -0,0 +1,43 @@
+# list1 = [5, 10, 15, 200, 25, 50, 20]
+# # Find the index of the first occurrence of 20 # Update the value at that index to 200
+# list1[list1.index(20)] = 200
+# print(list1)
+
+# a_tuple = (10, 20, 30, 40)
+# a, b, c, d = a_tuple
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+
+# user_num = int(input("Enter a number: "))
+# number = int(user_num)
+# for i in range(11):
+#     print(i * number)
+
+
+# while loop
+
+# x = 10
+# while x < 10:
+#     print(x)
+#     x += 1
+
+
+password = "ruben"
+
+# get_pass = input("Whats the password?")
+# while get_pass != password:
+#   print("Wrong! ")
+#   get_pass = input("Whats the password?")
+
+# print("you hacked the mainframe!")
+
+while True:
+    get_pass = input("What's the password")
+    if get_pass == password:
+        print("you hacked the mainframe!")
+        break
+    else:
+        print("Wrong!")
