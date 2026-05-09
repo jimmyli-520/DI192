8571fc32f419390341b3911769a4f5602dd1cb80
jimmyli-520 <jameskleinlee12@gmail.com>
2026-05-09 20:51:39 +0300

    dailychallenge BookApp

---
+++ week8/day2/dailychallenge/BookApp/src/App.tsx
@@ -0,0 +1,11 @@
+import BookApp from "./BookApp";
+
+function App() {
+  return(
+    <div>
+      <BookApp />
+    </div>
+  )
+}
+
+export default App;
---
+++ week8/day2/dailychallenge/BookApp/src/BookApp.tsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+
+type Book = {
+  id: number;
+  title: string;
+  author: string;
+}
+
+interface ListProps <T> {
+  items: T[];
+  renderItem:(item: T) => JSX.Element;
+}
+
+function List <T> ({items, renderItem}: ListProps<T>) {
+  return(
+    <ul>
+      {items.map((item, index) => (
+        <li key= {index} >
+          {renderItem(item)}
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+function BookApp() {
+  const [books, setBooks] = useState<Book[]>([
+    { id: 1, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
+    { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee' },
+    { id: 3, title: '1984', author: 'George Orwell' },
+  ])
+
+  function addBook() {
+    const newBook: Book = {
+      id: books.length + 1,
+      title: `Book ${books.length + 1}`,
+      author: `Author ${books.length + 1}`,
+    };
+    setBooks([...books, newBook]);
+  }
+
+  return (
+    <div>
+      <h2>Book List</h2>
+      <button onClick={addBook}>Add Book</button>
+      <List
+        items={books}
+        renderItem={(book) => (
+          <span>
+            <strong>{book.title}</strong> — {book.author}
+          </span>
+        )}
+      />
+    </div>
+  );
+}
+
+export default BookApp;
